8bedab23b5e5c5787aec79eca38f5b15fb6ffeb6
KingRacing <109063325+KingRacing@users.noreply.github.com>
2022-11-04 12:03:11 +0800

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,9 +3,9 @@ HEROKU = True # Make it False if you're not deploying on heroku.
 if HEROKU:
     from os import environ
 
-    bot_token = environ["bot_token"]
-    ARQ_API_KEY = environ["ARQ_API_KEY"]
-    LANGUAGE = environ["LANGUAGE"]
+    bot_token = environ["5445530721:AAFI2_zBd_zrhUS0_OHG6iYF9jIT9zn8SNc"]
+    ARQ_API_KEY = environ["MXJLGJ-LZCAJQ-VAAAYT-BDDDZK-ARQ"]
+    LANGUAGE = environ["id"]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
